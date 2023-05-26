ae2e3b9ca7b1107796ff48bd703a7fb2fb6bb827
Валерия Подопрыгина <podoprygina@MacBook-Air-Valeria.local>
2023-05-26 19:56:17 +0300

    with class ArabicToRoman

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Main{
     public static class RomantoArabicConverter {
         private HashMap<Character, Integer> romanDict = new HashMap<>();
+
         public RomantoArabicConverter() {
             romanDict.put('I', 1);
             romanDict.put('V', 5);
@@ -28,6 +29,8 @@ public class Main{
             }
             return result;
         }
+    }
+    public static class ArabicToRoman{
         public static String ArabicToRoman(int number) {
             LinkedHashMap<Integer, String> arabicDict = new LinkedHashMap<>();
             arabicDict.put(100, "C");
@@ -50,84 +53,84 @@ public class Main{
             }
             return result.toString();
         }
+    }
 
+    private static boolean isNumber(String s) throws Exception {
+        return s.matches("\\d+");
+    }
 
-        private static boolean isNumber(String s) throws  Exception{
-                return s.matches("\\d+");
+    private static boolean isRomanNumber(String s) {
+        return s.matches("[IVX]+");
+    }
+
+    public static String calc(String input) throws Exception {
+        RomantoArabicConverter converter = new RomantoArabicConverter();
+        ArabicToRoman arabicToRoman = new ArabicToRoman();
+        String[] elements = input.split(" ");
+
+        if (elements.length != 3)
+            throw new Exception("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+        String firstElement = elements[0];
+        String operation = elements[1];
+        String secondElement = elements[2];
+
+        if (!(isNumber(firstElement) && isNumber(secondElement)) &&
+                !(isRomanNumber(firstElement) && isRomanNumber(secondElement)))
+            throw new Exception("Exception //т.к. используются одновременно разные системы счисления");
+        int a = 0, b = 0;
+        if (isNumber(firstElement)) {
+            a = Integer.parseInt(firstElement);
+            if (a < 1 || a > 10)
+                throw new Exception("Только арабские цифры в диапазоне от 1 до 10 включительно");
+        } else {
+            a = converter.convert(firstElement);
+            if (a < 1 || a > 10)
+                throw new Exception("Только числа от 1 до 10 в десятичной римской системе.");
         }
-        private static boolean isRomanNumber(String s) {
-            return s.matches("[IVX]+");
-        }
-        public static String calc(String input) throws Exception {
-            RomantoArabicConverter converter = new RomantoArabicConverter();
-            String[] elements = input.split(" ");
-
-            if (elements.length != 3)
-                throw new Exception("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
-            String firstElement = elements[0];
-            String operation = elements[1];
-            String secondElement = elements[2];
-
-            if (!(isNumber(firstElement) && isNumber(secondElement)) &&
-                    !(isRomanNumber(firstElement) && isRomanNumber(secondElement)))
-                throw new Exception("Exception //т.к. используются одновременно разные системы счисления");
-            int a = 0, b = 0;
-            if (isNumber(firstElement)) {
-                a = Integer.parseInt(firstElement);
-                if (a < 1 || a > 10)
-                    throw new Exception("Только арабские цифры в диапазоне от 1 до 10 включительно");
-            } else {
-                a = converter.convert(firstElement);
-                if (a < 1 || a > 10)
-                    throw new Exception("Только числа от 1 до 10 в десятичной римской системе.");
-            }
-            if (isNumber(secondElement)) {
-                b = Integer.parseInt(secondElement);
-                if (b < 1 || b > 10)
-                    throw new Exception("Только арабские цифры в диапазоне от 1 до 10 включительно");
-            } else {
-                b = converter.convert(secondElement);
-                if (b < 1 || b > 10)
-                    throw new Exception("Только числа от 1 до 10 в десятичной римской системе.");
-            }
-
-            int result = 0;
-            switch (operation) {
-                case "+":
-                    result = a + b;
-                    break;
-                case "-":
-                    result = a - b;
-                    break;
-                case "*":
-                    result = a * b;
-                    break;
-                case "/":
-                    result = a / b;
-                    break;
-                default:
-                    throw new Exception("Неизвестная арифметическая операция.");
-            }
-
-            if (isNumber(firstElement) && isNumber(secondElement)) {
-                return Integer.toString(result);
-            }
-            else {
-                if (result <= 0)
-                    throw new Exception("throws Exception //т.к. в римской системе нет отрицательных чисел");
-                return ArabicToRoman(result);
-            }
+        if (isNumber(secondElement)) {
+            b = Integer.parseInt(secondElement);
+            if (b < 1 || b > 10)
+                throw new Exception("Только арабские цифры в диапазоне от 1 до 10 включительно");
+        } else {
+            b = converter.convert(secondElement);
+            if (b < 1 || b > 10)
+                throw new Exception("Только числа от 1 до 10 в десятичной римской системе.");
         }
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            try {
-                String input = scanner.nextLine();
-                System.out.println(calc(input));
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.exit(1);
-            }
+        int result = 0;
+        switch (operation) {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                result = a / b;
+                break;
+            default:
+                throw new Exception("Неизвестная арифметическая операция.");
+        }
+        if (isNumber(firstElement) && isNumber(secondElement)) {
+            return Integer.toString(result);
+        } else {
+            if (result <= 0)
+                throw new Exception("throws Exception //т.к. в римской системе нет отрицательных чисел");
+            return arabicToRoman.ArabicToRoman(result);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            String input = scanner.nextLine();
+            System.out.println(calc(input));
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
     }
 }
